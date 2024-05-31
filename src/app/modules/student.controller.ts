385825e9b0b9bb07616ce3c4ab31e374a85303fe
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2024-05-31 22:22:10 +0600

    student find by data modified

--- src/app/modules/student.controller.ts
+++ src/app/modules/student.controller.ts
@@ -18,6 +18,38 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students is successfully retrieved',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is found successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentController = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 };
