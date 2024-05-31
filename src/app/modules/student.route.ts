c4e09ce188f628dd11657aac50391fc5f7394c13
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2024-05-31 06:53:56 +0600

    create student data sucessfully

--- src/app/modules/student.route.ts
+++ src/app/modules/student.route.ts
@@ -4,3 +4,5 @@ import { StudentController } from './student.controller';
 const router = express.Router();
 // will call controller function
 router.post('/create-student', StudentController.createStudent);
+
+export const StudentRoutes = router;
